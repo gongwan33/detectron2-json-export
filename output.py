a47cd6dfe602452b9e6ecd9e83a2ab54acdc370b
wagner <gongwan33@gmail.com>
2019-12-02 15:27:11 +1100

    fix json no comma bug

--- output.py
+++ output.py
@@ -12,15 +12,17 @@ class JSONOutput:
 
         print("Open %s to write json data"%ofname)
         self.f = open(ofname, 'w+')
+
         if self.f is not None:
             self.f.write("[")
+        
+        self.anno_count = 0
         return
 
     def write(self, predictions, idx):
         instances = predictions
         keypoints = instances.pred_keypoints.numpy().astype("float").round(5)
         scores = instances.scores.numpy()
-        anno_count = 0
 
         for i, points in enumerate(keypoints):
             key_annotation = {
@@ -31,11 +33,11 @@ class JSONOutput:
             }
     
             if self.f is not None:
-                if anno_count > 0:
+                if self.anno_count > 0:
                     self.f.write(",\n")
  
                 json.dump(key_annotation, self.f)
-                anno_count += 1
+                self.anno_count += 1
     
         return
     
