18630f979726a820aafa059802f117a2c039a6d1
wagner <gongwan33@gmail.com>
2019-12-02 13:02:01 +1100

    first commit

---
+++ output.py
@@ -0,0 +1,13 @@
+def json_output(predictions):
+    print(predictions)
+    instances = predictions['instances']
+    keypoints = instances.pred_keypoints
+    print(instances)
+    print(keypoints)
+
+def json_output_init():
+    return
+
+def json_output_finish():
+    return
+
