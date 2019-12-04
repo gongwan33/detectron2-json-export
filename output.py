d4e1ad690a34374df577153c6ef92c9e50b3607f
wagner <gongwan33@gmail.com>
2019-12-05 09:38:27 +1100

    add gpu info thread

--- output.py
+++ output.py
@@ -1,5 +1,8 @@
 import json
 import numpy as np
+from gpuinfo import GPUInfo
+import _thread
+import time
 
 class JSONOutput:
     def __init__(self, fname):
@@ -17,6 +20,29 @@ class JSONOutput:
             self.f.write("[")
         
         self.anno_count = 0
+        self.gpu_percent = 0
+        self.gpu_memory = 0
+        self.gpu_thread = True
+        self.thread_lock = _thread.allocate_lock()
+
+        try:
+            _thread.start_new_thread(self.get_gpu_info, ())
+            print("GPUINFO Thread Started.")
+        except Exception as e:
+            print("Unable to start GPUINFO Thread")
+            print(e)
+
+        return
+
+    def get_gpu_info(self):
+        print("GPUINFO: Start loop")
+        while self.gpu_thread:
+            self.thread_lock.acquire()
+            self.gpu_percent, self.gpu_memory = GPUInfo.gpu_usage()
+            self.thread_lock.release()
+            time.sleep(1)
+
+        print("GPUINFO: End loop")
         return
 
     def write(self, predictions, idx):
@@ -25,12 +51,17 @@ class JSONOutput:
         scores = instances.scores.numpy()
 
         for i, points in enumerate(keypoints):
+            self.thread_lock.acquire()
             key_annotation = {
                 "image_id": idx,
                 "category_id": 1,
                 "keypoints": points.flatten().tolist(),
-                "score": round(scores[i].astype("float"), 5)
+                "score": round(scores[i].astype("float"), 5),
+                "instance_num": len(keypoints),
+                "gpu_percentage": self.gpu_percent,
+                "gpu_memory": self.gpu_memory
             }
+            self.thread_lock.release()
     
             if self.f is not None:
                 if self.anno_count > 0:
@@ -45,5 +76,6 @@ class JSONOutput:
         if self.f is not None:
             self.f.write("]")
             self.f.close()
+            self.gpu_thread = False
         return
 
